01d2948bf6ef6aec5886ed01bf5421b80a5dc029
diana7770 <20diane09@gmail.com>
2023-03-31 00:27:47 +0300

    дз2

--- js/index1.js
+++ js/index1.js
@@ -1,3 +1,16 @@
-const message = "Я Діана"; //7
-const messageLenght = message.length;
-console.log(messageLenght);
+// const message = "Я Діана"; //7
+// const messageLenght = message.length;
+// console.log(messageLenght);
+
+const userName = prompt("Введіть ваше ім'я");
+console.log(userName);
+
+const myAge = 13;
+const friendAge = 12;
+const resultAge = myAge + friendAge;
+alert(resultAge);
+console.log(resultAge);
+
+const message = "Я навчаюся в GoITeens в групі:";
+const group = "FE1y_1 менші";
+console.log(message, group);
--- js/index2.js
+++ js/index2.js
@@ -1,2 +1,5 @@
-const username = "Shulga";
-console.log("Username is ", username);
+// const username = "Shulga";
+// console.log("Username is ", username);
+const productName = "MacBook Pro 13";
+const price = 10000;
+console.log(`Обрано ${productName}, ціна за штуку ${price} кредитів`);
