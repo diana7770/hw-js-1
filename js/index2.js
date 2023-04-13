fc2fc322a1d50e4fbe931e3f8262be3ea6d8e699
diana7770 <20diane09@gmail.com>
2023-04-13 23:35:52 +0300

    дз 4

--- js/index2.js
+++ js/index2.js
@@ -3,38 +3,69 @@
 // const productName = "MacBook Pro 13";
 // const price = 10000;
 // console.log(`Обрано ${productName}, ціна за штуку ${price} кредитів`);
-let k1 = "5px";
-k1 = Number.parseInt(k1);
-console.log(k1);
-let k2 = "12djd334";
-k2 = Number.parseInt(k2);
-console.log(k2);
-let k3 = "12.45asdwe12";
-k3 = Number.parseInt(k3);
-console.log(k3);
-let k4 = "qwqweeewq";
-k4 = Number.parseInt(k4);
-console.log(k4);
+// let k1 = "5px";
+// k1 = Number.parseInt(k1);
+// console.log(k1);
+// let k2 = "12djd334";
+// k2 = Number.parseInt(k2);
+// console.log(k2);
+// let k3 = "12.45asdwe12";
+// k3 = Number.parseInt(k3);
+// console.log(k3);
+// let k4 = "qwqweeewq";
+// k4 = Number.parseInt(k4);
+// console.log(k4);
 
-let K1 = "5px";
-K1 = Number.parseFloat(K1);
-console.log(K1);
-let K2 = "12djd334";
-K2 = Number.parseFloat(K2);
-console.log(K2);
-let K3 = "12.45asdwe12";
-K3 = Number.parseFloat(K3);
-console.log(K3);
-let K4 = "qwqweeewq";
-K4 = Number.parseFloat(K4);
-console.log(K4);
+// let K1 = "5px";
+// K1 = Number.parseFloat(K1);
+// console.log(K1);
+// let K2 = "12djd334";
+// K2 = Number.parseFloat(K2);
+// console.log(K2);
+// let K3 = "12.45asdwe12";
+// K3 = Number.parseFloat(K3);
+// console.log(K3);
+// let K4 = "qwqweeewq";
+// K4 = Number.parseFloat(K4);
+// console.log(K4);
 
-const bigNum = Math.max(2, 34, 99, 3, 22, 36, 733, 18);
-console.log(bigNum);
-const smallNum = Math.min(2, 34, 99, 3, 22, 36, 733, 18);
-console.log(smallNum);
+// const bigNum = Math.max(2, 34, 99, 3, 22, 36, 733, 18);
+// console.log(bigNum);
+// const smallNum = Math.min(2, 34, 99, 3, 22, 36, 733, 18);
+// console.log(smallNum);
 
-const maxNum = 19;
-const minNum = 3;
-const num = Math.round(Math.random() * (maxNum - minNum) + minNum);
-console.log(num);
+// const maxNum = 19;
+// const minNum = 3;
+// const num = Math.round(Math.random() * (maxNum - minNum) + minNum);
+// console.log(num);
+
+const pricePerItem = 3500;
+const orderedQuantity = 4;
+const totalPrice = pricePerItem * orderedQuantity;
+console.log(totalPrice);
+
+const passed = prompt("Скільки Вам років? ");
+const age = passed > 18;
+console.log(age);
+
+const SAVED_PASSWORD = "Oleg lox)";
+const password = prompt("Введіть пароль!!!");
+const isMatch = SAVED_PASSWORD === password;
+console.log(isMatch);
+
+let a = 5;
+a += 2;
+
+let b = 10;
+b -= 4;
+
+let c = 15;
+c *= 3;
+
+let d = 20;
+d /= 10;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
