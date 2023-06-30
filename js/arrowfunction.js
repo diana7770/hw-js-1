4713654082198669038df3c4a3fb4ce1c62e497d
diana7770 <20diane09@gmail.com>
2023-07-01 00:13:37 +0300

    object

--- js/arrowfunction.js
+++ js/arrowfunction.js
@@ -1,19 +1,15 @@
-//1
-const logItems = (array) => {
+//dz 11:
+const logItems = function (array) {
   for (let i = 0; i < array.length; i += 1) {
     console.log(`${i + 1} - ${array[i]}`);
   }
 };
-
 logItems(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]);
 logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
 
 //2
-const calculateEngravingPrice = (message, pricePerWord) => {
-  const words = message.split(" ");
-  const total = words.length * pricePerWord;
-  return total;
-};
+const calculateEngravingPrice = (message, pricePerWord) =>
+  message.split(" ").length * pricePerWord;
 
 console.log(
   calculateEngravingPrice(
@@ -38,7 +34,7 @@ console.log(
 ); // 120.
 
 //3
-const findLongestWord = (string, longestWord) => {
+const findLongestWord = (string) => {
   const words = string.split(" ");
   let longestWord = "";
   for (let i = 0; i < words.length; i += 1) {
@@ -46,22 +42,16 @@ const findLongestWord = (string, longestWord) => {
       longestWord = words[i];
     }
   }
+  return longestWord;
 };
 
 console.log(findLongestWord("The quick brown fox jumped over the lazy dog")); // 'jumped'
 
 console.log(findLongestWord("Google do a roll")); // 'Google'
 
-console.log(findLongestWord("May the force be with you")); // 'force'.
-
 //4
-const formatString = function (string) {
-  if (string.length <= 40) {
-    return string;
-  } else {
-    return string.slice(0, 40) + "...";
-  }
-};
+const formatString = (string) =>
+  string.length <= 40 ? string : string.slice(0, 40) + "...";
 
 console.log(formatString("Curabitur ligula sapien, tincidunt non.")); // 'Curabitur ligula sapien, tincidunt non.'
 
@@ -75,8 +65,8 @@ console.log(
   )
 ); // 'Nunc sed turpis. Curabitur a felis in nunc fringil...'
 
-//5
-const checkForSpam = function (message) {
+//5 доробити
+const checkForSpam = (message) => {
   const normalizedMessage = message.toLowerCase();
   return (
     normalizedMessage.includes("spam") || normalizedMessage.includes("sale")
@@ -90,6 +80,61 @@ console.log(checkForSpam("JavaScript weekly newsletter")); // false
 console.log(checkForSpam("Get best sale offers now!")); // true
 
 console.log(checkForSpam("[SPAM] How to earn fast money?")); // true.
+//dz12
 
-//6
-//-
+//1
+const makePizza = () => "Your pizza is being prepared, please wait.";
+
+// Change code below this line
+
+const result = console.log(
+  makePizza("Your pizza is being prepared, please wait.")
+);
+
+const pointer = console.log(makePizza);
+
+//3
+// function deliverPizza(pizzaName) {
+//   return `Delivering ${pizzaName} pizza.`;
+// }
+
+// function makePizza(pizzaName) {
+//   return `Pizza ${pizzaName} is being prepared, please wait...`;
+// }
+
+// // Chande code below this line
+
+// function makeMessage(pizzaName, callback) {
+//   return callback(pizzaName);
+//}
+
+//4
+const buttonRef = document.querySelector("[data-button]");
+function showInfo() {
+  return alert("Hello World!");
+}
+buttonRef.addEventListener("click", showInfo);
+
+// 5
+// const buttonref = document.querySelector("[data-button]");
+// let click = 0;
+
+// function buttonClickNum() {
+//   click += 1;
+//   console.log(click);
+// }
+
+// buttonref.addEventListener("click", buttonClickNum);
+
+// 7
+
+// const calculateDiscountedPrice = function(price, discount, callback) {
+//   const discountedPrice = price * (1 - discount / 100);
+//   return callback(discountedPrice);
+// }
+
+// const showDiscountedPrice = function(discountedPrice) {
+//   return Discounted price: ${discountedPrice};
+// }
+
+// console.log(calculateDiscountedPrice(100, 10, showDiscountedPrice)); // Discounted price: 90
---
+++ js/object.js
@@ -0,0 +1,96 @@
+//1
+// const myArray = {};
+// myArray.name: "Ira";
+// myArray.age: "55";
+// myArray.city: "Lviv";
+// myArray.occupetion: "ingeniur";
+// console.log(myArray.friand);
+
+//2
+// const basket = {
+//   clothes: [
+//     {
+//       dress: 1,
+//     },
+//     {
+//       jeans: ["pink", "black", "white"],
+//     },
+//     {
+//       socks: true,
+//     },
+//     {
+//       blouse: null,
+//     },
+//   ],
+// };
+// const dress = basket.clothes.dress;
+// console.log(dress);
+//...
+
+// //3
+// //1
+// const apartament = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+//   //2
+//   owner: {
+//     name: "Henry",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+// //3
+// const aptRating = apartament.rating;
+// const aptDescr = apartament.descr;
+// const aptPrice = apartament.price;
+// const aptTags = apartament.tags;
+// //4
+// const ownerName = apartament.owner.name;
+// const ownerPhone = apartament.owner.phone;
+// const ownerEmail = apartament.owner.email;
+// const numberOfTags = apartament.tags.length;
+// const firstTag = apartament.tags[0];
+// const lastTag = apartament.tags[apartament.tags.length - 1];
+// //5
+// const aptRating2 = apartament["rating"];
+// const aptDescr2 = apartament["descr"];
+// const aptPrice2 = apartament["price"];
+// const aptTags2 = apartament["tags"];
+// //6
+// apartament["price"] = 5000;
+// apartament["rating"] = 4.7;
+// apartament.owner.name = "Henry Sibola";
+// apartament.tags.push("trusted");
+// //7
+// apartment.area = 60;
+// apartment.rooms = 3;
+// apartment.location = {
+//   country: "Jamaica",
+//   city: "Kingston",
+// };
+
+//4
+// const name = "Repair Droid";
+// const price = 2500;
+// const image = "https://via.placeholder.com/640x480";
+// const tags = ["on sale", "trending", "best buy"];
+
+// const product = {
+//   name,
+//   price,
+//   image,
+//   tags,
+// };
+
+//5
+// const emailInputName = "email";
+
+// const passwordInputName = "password";
+
+// const credentials = {
+//   [emailInputName]: "henry.carter@aptmail.com",
+//   [passwordInputName]: "jqueryismyjam",
+// };
